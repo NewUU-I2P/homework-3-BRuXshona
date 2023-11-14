85ef122e2f5fdce37c44dbf8951be4eb39e83719
macbookpro <ruxshonabrdmr@gmail.com>
2023-11-14 12:58:07 +0500

    qwerty

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,23 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-
+    if(S == 'M'){
+        if (height >= 185){
+            return "Tall";
+        }else if (height < 170){
+            return "Short";
+        }else {
+            return "Normal";
+        }
+    }else {
+        if (height >= 175){
+            return "Tall";
+        }else if (height < 160){
+            return "Short";
+        }else {
+            return "Normal";
+        }
+    }
     // use return to return your result
     // make use of control flow statements
 }
--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,7 +3,36 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
-
+    if(macAddress[0] == 'F' and macAddress[1] == 'F'){
+        return "Broadcast";
+    }else{
+        switch(macAddress[1]){
+            case 'A':
+                return "Unicast";
+                break;
+            case 'B':
+                return "Multicast";
+                break;
+            case 'C':
+                return "Unicast";
+                break;
+            case 'D':
+                return "Multicast";
+                break;
+            case 'E':
+                return "Unicast";
+                break;
+            case 'F':
+                return "Multicast";
+                break;
+            default:
+                if((macAddress[1] - '0') % 2 == 0){
+                    return "Unicast";
+                }else {
+                    return "Multicast";
+                }
+        }
+    }
     // make use of control flow statements
     // return result;
 }
